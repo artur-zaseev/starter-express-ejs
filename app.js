e854b3a9ad0c3b0632f3b99c3b9f442fd56a4061
Засеев Артур <zaseevav@it2g.ru>
2022-10-16 12:54:21 +0300

    init

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
+const PORT = 3000;
+
+const app = express();
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('pages/home', {
+    title: 'Main page',
+    text: 'This is main page',
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('pages/about', {
+    title: 'About page',
+    text: 'This is about page',
+  });
+});
+
+app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
